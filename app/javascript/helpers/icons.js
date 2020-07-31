e6de3a4fc5e77a1f39f5670650adfd5da47a1676
Tina <tinavanschelt@gmail.com>
2020-07-31 18:47:36 +0200

    add icons

---
+++ app/javascript/helpers/icons.js
@@ -0,0 +1,6 @@
+import iconFlag from '../../assets/images/icon-flag.png'
+import iconSmile from '../../assets/images/icon-smile.png'
+import iconCalendar from '../../assets/images/icon-calendar.png'
+import iconPlus from '../../assets/images/icon-plus.png'
+
+export { iconFlag, iconSmile, iconCalendar, iconPlus }
