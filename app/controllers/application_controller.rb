24d39e1345a6fe7a2c7031e6162b4f71c8948fe8
Tina <tinavanschelt@gmail.com>
2020-08-02 13:29:19 +0200

    ensure use is signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  def ensure_signed_in
+    return nil if signed_in?
+
+    redirect_to new_user_session_path
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  before_action :ensure_signed_in
+  
   def dashboard
   end
 end
