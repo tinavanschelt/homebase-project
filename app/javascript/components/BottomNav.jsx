05467cb91521a3fbe6b88c7f44b3150b6595a898
Tina <tinavanschelt@gmail.com>
2020-08-02 16:55:32 +0200

    add nav popup and cleanup border-radius variables

--- app/javascript/components/BottomNav.jsx
+++ app/javascript/components/BottomNav.jsx
@@ -1,27 +1,66 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { iconFlag, iconSmile, iconCalendar, iconPlus } from '../helpers/icons'
+import {
+    iconFlag,
+    iconSmile,
+    iconCalendar,
+    iconPlus,
+    iconPlusInverted,
+} from '../helpers/icons'
 
-const BottomNav = () => (
-    <div className="bottom-nav">
-        <span className="bottom-nav__item">
-            <img src={iconFlag} alt="Home" />
-            Home
-        </span>
-        <span className="bottom-nav__item">
-            <img src={iconSmile} alt="My Stuff" />
-            My Stuff
-        </span>
-        <span className="bottom-nav__item">
-            <img src={iconCalendar} alt="Calendar" />
-            Calendar
-        </span>
-        <span className="bottom-nav__item">
-            <img src={iconPlus} alt="New" />
-            New
-        </span>
-    </div>
-)
+class BottomNav extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { popupOpen: false }
+        this.togglePopup = this.togglePopup.bind(this)
+    }
+
+    togglePopup() {
+        this.setState({ popupOpen: !this.state.popupOpen })
+    }
+
+    render() {
+        const { popupOpen } = this.state
+
+        return (
+            <div className="bottom-nav">
+                {popupOpen && (
+                    <div className="bottom-nav__pop-up">
+                        <div className="bottom-nav__pop-up__item">
+                            <img src={iconPlusInverted} alt="New Event" />
+                            Create New Event
+                        </div>
+                        <div className="bottom-nav__pop-up__item">
+                            <img src={iconPlusInverted} alt="New Task" />
+                            Create New Task
+                        </div>
+                    </div>
+                )}
+                <div className="bottom-nav__items">
+                    <span className="bottom-nav__items__item">
+                        <img src={iconFlag} alt="Home" />
+                        Home
+                    </span>
+                    <span className="bottom-nav__items__item">
+                        <img src={iconSmile} alt="My Stuff" />
+                        My Stuff
+                    </span>
+                    <span className="bottom-nav__items__item">
+                        <img src={iconCalendar} alt="Calendar" />
+                        Calendar
+                    </span>
+                    <span
+                        className="bottom-nav__items__item"
+                        onClick={() => this.togglePopup()}
+                    >
+                        <img src={iconPlus} alt="New" />
+                        New
+                    </span>
+                </div>
+            </div>
+        )
+    }
+}
 
 BottomNav.propTypes = {}
 
--- app/javascript/helpers/icons.js
+++ app/javascript/helpers/icons.js
@@ -2,5 +2,6 @@ import iconFlag from '../../assets/images/icon-flag.png'
 import iconSmile from '../../assets/images/icon-smile.png'
 import iconCalendar from '../../assets/images/icon-calendar.png'
 import iconPlus from '../../assets/images/icon-plus.png'
+import iconPlusInverted from '../../assets/images/icon-plus-inverted.png'
 
-export { iconFlag, iconSmile, iconCalendar, iconPlus }
+export { iconFlag, iconSmile, iconCalendar, iconPlus, iconPlusInverted }
