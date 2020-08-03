31aeda0d21c8a242c41008b4e987d4dc9d46b31e
Tina <tinavanschelt@gmail.com>
2020-08-04 00:03:59 +0200

    typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,6 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "homebase_production"
 
   config.action_mailer.perform_caching = false
-  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
