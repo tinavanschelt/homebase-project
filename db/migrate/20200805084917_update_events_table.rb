ed6c522b3748ae37abf94147b54139f6d55cb50b
Tina <tinavanschelt@gmail.com>
2020-08-06 11:15:31 +0200

    update db migrations

--- db/migrate/20200805084917_update_events_table.rb
+++ db/migrate/20200805084917_update_events_table.rb
@@ -1,6 +1,5 @@
 class UpdateEventsTable < ActiveRecord::Migration[6.0]
   def change
-    rename_column :events, :user_id, :user_id
     add_column :events, :group_id, :integer
   end
 end
--- db/migrate/20200805084923_update_tasks_table.rb
+++ db/migrate/20200805084923_update_tasks_table.rb
@@ -1,6 +1,5 @@
 class UpdateTasksTable < ActiveRecord::Migration[6.0]
   def change
-    rename_column :tasks, :user_id, :user_id
     add_column :tasks, :group_id, :integer
   end
 end
